fadd1f8b69938a479ab1436eccaf504ae48bf3c0
Tom-Inge Larsen <tom-inge.larsen@advania.no>
2018-04-15 22:32:08 +0200

    Added examples using module functions

--- example.ps1
+++ example.ps1
@@ -6,3 +6,28 @@ Set-I2CRegister -Device $dev -Register 0x08 -Data 0x1f
 Set-I2CRegister -Device $dev -Register 0xa -Data 0xbb
 Set-I2CRegister -Device $dev -Register 0xc -Data 0xb1
 Set-I2CRegister -Device $dev -Register 0xe -Data 0xa0
+
+Set-Ht16k33LedOn 0 6
+Set-Ht16k33LedOn 1 6
+Set-Ht16k33LedOn 2 3
+Set-Ht16k33LedOn 2 6
+Set-Ht16k33LedOn 3 2
+Set-Ht16k33LedOn 3 3
+Set-Ht16k33LedOn 3 4
+Set-Ht16k33LedOn 4 1
+Set-Ht16k33LedOn 4 2
+Set-Ht16k33LedOn 4 3
+Set-Ht16k33LedOn 4 4
+Set-Ht16k33LedOn 4 5
+Set-Ht16k33LedOn 5 0
+Set-Ht16k33LedOn 5 1
+Set-Ht16k33LedOn 5 2
+Set-Ht16k33LedOn 5 4
+Set-Ht16k33LedOn 5 5
+Set-Ht16k33LedOn 5 6
+Set-Ht16k33LedOn 6 0
+Set-Ht16k33LedOn 6 1
+Set-Ht16k33LedOn 6 5
+Set-Ht16k33LedOn 6 6
+Set-Ht16k33LedOn 7 0
+Set-Ht16k33LedOn 7 6
