5fb8ef9c2106a26dbbfa19ea11390b99859b3537
Tom-Inge Larsen <tom-inge.larsen@advania.no>
2018-05-04 01:00:51 +0200

    string quotes

--- build.ps1
+++ build.ps1
@@ -41,7 +41,7 @@ function Resolve-Module {
 # Grab nuget bits, install modules, set build variables, start build.
 Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
 
-Resolve-Module Psake, PSDeploy, Pester, BuildHelpers, Microsoft.Powershell.IoT
+Resolve-Module "Psake", "PSDeploy", "Pester", "BuildHelpers", "Microsoft.Powershell.IoT"
 
 Set-BuildEnvironment
 
