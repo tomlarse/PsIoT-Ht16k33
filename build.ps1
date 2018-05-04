6d905c6191eaec0fc0d7d58e6c6e2c002a268290
Tom-Inge Larsen <tom-inge.larsen@advania.no>
2018-05-04 11:43:30 +0200

    Appveyor deploy test 2

--- build.ps1
+++ build.ps1
@@ -39,7 +39,7 @@ function Resolve-Module {
 }
 
 # Grab nuget bits, install modules, set build variables, start build.
-# Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
+Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
 
 Resolve-Module "Psake", "PSDeploy", "Pester", "BuildHelpers", "Microsoft.Powershell.IoT"
 
